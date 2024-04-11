78f3bb9c8a1888569dae365013e981e5f1c58e96
kainrii <117970207+lydk-0326@users.noreply.github.com>
2024-04-12 01:00:04 +0700

    set up CORS

---
+++ backend-app/Services/ConfigureServicesBase.cs
@@ -0,0 +1,21 @@
+﻿namespace BackendApp.Services
+{
+    public class ConfigureServicesBase
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add CORS service
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", builder =>
+                    builder.WithOrigins("http://localhost:3000")  // Replace with the client URL
+                           .AllowAnyMethod()
+                           .AllowAnyHeader()
+                           .AllowCredentials());
+            });
+
+            // Add framework services.
+            services.AddControllers();
+        }
+    }
+}
