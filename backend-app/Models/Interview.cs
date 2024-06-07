ab08821860ae9bdf628e888ed8493640509fd7e1
kainrii <117970207+lydk-0326@users.noreply.github.com>
2024-06-07 16:32:12 +0700

    interview model

---
+++ backend-app/Models/Interview.cs
@@ -0,0 +1,31 @@
+﻿namespace BackendApp.Models
+{
+    public class Interview
+    {
+        public DateTime Date { get; set; }
+        public string Time { get; set; }
+        public string TalentName { get; set; }
+        public string TalentId { get; set; }
+        public string CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string Interviewers { get; set; }
+        public string Location { get; set; }
+        public string Status { get; set; } 
+        public List<CompanyFeedback> CompanyFeedbacks { get; set; } 
+        public List<TalentFeedback> TalentFeedbacks { get; set; } 
+
+        public class CompanyFeedback
+        {
+            public string Username { get; set; }
+            public string Content { get; set; }
+            
+        }
+
+        public class TalentFeedback
+        {
+            public string Username { get; set; }
+            public string Content { get; set; }
+
+        }
+    }
+}
